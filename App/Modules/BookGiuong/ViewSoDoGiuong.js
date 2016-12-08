a033b22e3b013697c28c2c430f12409605026baa
tuanphan <tuanpm.deverloper@gmail.com>
2016-12-08 18:14:51 +0700

    .

--- App/Modules/BookGiuong/ViewSoDoGiuong.js
+++ App/Modules/BookGiuong/ViewSoDoGiuong.js
@@ -68,6 +68,7 @@ class ViewSoDoGiuong extends Component {
 		});
 		setTimeout(() => {
 			var apiUrl = domain+'/api/api_adm_so_do_giuong.php?not_id='+this.props.data.notId+'&day='+this.props.data.day;
+			console.log(apiUrl);
 			fetch(apiUrl, {
 				headers: {
 			    	'Cache-Control': cache
@@ -115,10 +116,10 @@ class ViewSoDoGiuong extends Component {
 			default:
 		}
 		if(dataTang != undefined) {
-			for(var i = 1; i <= Object.keys(dataTang).length; i++) {
+			for(var i in dataTang) {
 				var item = dataTang[i];
 				var htmlChild = [];
-				for(var j = 1; j <= Object.keys(item).length; j++) {
+				for(var j in item) {
 					// if(item[j].sdgct_disable == '1') {
 					// 	htmlChild.push( <Col key={i+j} style={styles.nullBorderCol}></Col> );
 					// }else {
@@ -346,6 +347,7 @@ class ViewSoDoGiuong extends Component {
 					type: ''
 				});
 				var that = this;
+
 				fetch(domain+'/api/api_adm_ben.php?type=getBen&notTuyenId='+this.props.data.notTuyenId, {
 					headers: {
 				    	'Cache-Control': cache
@@ -354,6 +356,7 @@ class ViewSoDoGiuong extends Component {
 				.then((response) => response.json())
 				.then((responseJson) => {
 					that.setState({
+						status: responseJson.status,
 						resultsBen: responseJson.dataBen,
 						bvv_bvn_id_muon_chuyen: dataGiuong.bvv_bvn_id,
 						bvv_number_muon_chuyen: dataGiuong.bvv_number,
@@ -402,118 +405,120 @@ class ViewSoDoGiuong extends Component {
 	}
 
 	_renderModalBen(data) {
-		let html = [],
-		listItem1 = [],
-		listItem2 = [],
-		keyDiemDi = this.state.keyDiemDi,
-		keyDiemDen = this.state.keyDiemDen,
-		currentDiemDen = '',
-		currentDiemDi = '',
-		type = this.state.type,
-		totalPriceInt = this.state.totalPriceInt;
+		let html = [];
+		if(this.state.status == 1) {
+			let listItem1 = [],
+			listItem2 = [],
+			keyDiemDi = this.state.keyDiemDi,
+			keyDiemDen = this.state.keyDiemDen,
+			currentDiemDen = '',
+			currentDiemDi = '',
+			type = this.state.type,
+			totalPriceInt = this.state.totalPriceInt;
 
-		if(this.state.nameDiemDen != '') {
-			currentDiemDen = this.state.nameDiemDen;
-		}
+			if(this.state.nameDiemDen != '') {
+				currentDiemDen = this.state.nameDiemDen;
+			}
 
-		if(this.state.nameDiemDi != '') {
-			currentDiemDi = this.state.nameDiemDi;
-		}
+			if(this.state.nameDiemDi != '') {
+				currentDiemDi = this.state.nameDiemDi;
+			}
 
-		if(this.props.data.bvh_id_can_chuyen != undefined && this.props.data.bvh_id_can_chuyen > 0) {
-			html.push(<Button key="9" block info style={styles.marginTopButton} onPress={this._handleXacNhanChuyenVaoCho.bind(this)}>Xác nhận Chuyển vào chỗ</Button>);
-		}else {
-			if(this.state.bvv_id_can_chuyen <= 0) {
-				if(Object.keys(data).length > 0) {
-					let dataGiuong = this.state.arrVeNumber[this.state.currentIdGiuong],
-						currentPrice = dataGiuong.bvv_price,
-						priceConver = 0;
+			if(this.props.data.bvh_id_can_chuyen != undefined && this.props.data.bvh_id_can_chuyen > 0) {
+				html.push(<Button key="9" block info style={styles.marginTopButton} onPress={this._handleXacNhanChuyenVaoCho.bind(this)}>Xác nhận Chuyển vào chỗ</Button>);
+			}else {
+				if(this.state.bvv_id_can_chuyen <= 0) {
+					if(Object.keys(data).length > 0) {
+						let dataGiuong = this.state.arrVeNumber[this.state.currentIdGiuong],
+							currentPrice = dataGiuong.bvv_price,
+							priceConver = 0;
+							if(type == 'update') {
+								if(totalPriceInt > 0) {
+									currentPrice = totalPriceInt;
+								}
+								if(currentPrice > 0) {
+									priceConver = currentPrice.toFixed(0).replace(/./g, function(c, i, a) {
+										return i && c !== "." && ((a.length - i) % 3 === 0) ? ',' + c : c;
+									});
+								}
+							}else {
+								if(totalPriceInt > 0) {
+									priceConver = totalPriceInt.toFixed(0).replace(/./g, function(c, i, a) {
+										return i && c !== "." && ((a.length - i) % 3 === 0) ? ',' + c : c;
+									});
+								}
+							}
+						Object.keys(data).map(function(key) {
+							let checkSelect = false;
+							if(keyDiemDi != '' && keyDiemDi == key) {
+								checkSelect = true;
+								if(type == 'update') {
+									currentDiemDi = data[key];
+								}
+							}
+							listItem1.push({key: key.toString(), section: checkSelect, label: data[key], value: key});
+						});
+
+						Object.keys(data).map(function(key) {
+							let checkSelect = false;
+							if(keyDiemDen != '' && keyDiemDen == key) {
+								checkSelect = true;
+								if(type == 'update') {
+									currentDiemDen = data[key];
+								}
+							}
+							listItem2.push({key: key.toString(), section: checkSelect, label: data[key], value: key});
+						});
+
+						html.push(
+							<Text key="1" style={{marginTop: 20, width: 200}}>Điểm đi:</Text>
+						);
+						html.push(
+							<ModalPicker
+							key="2"
+							data={listItem1}
+							initValue="Chọn điểm đi"
+							onChange={(option)=>{this.renderPriceBenDi(option)}}>
+							<TextInput
+							style={{borderWidth:1, borderColor:'#ccc', padding:10,  width: 200, height:30, marginTop: 10, marginBottom: 10}}
+							editable={false}
+							placeholder="Vui lòng chọn điểm đi"
+							value={currentDiemDi} />
+							</ModalPicker>
+						);
+						html.push(
+							<Text key="3" style={{width: 200}}>Điểm đến:</Text>
+						);
+						html.push(
+							<ModalPicker
+							key="4"
+							data={listItem2}
+							initValue="Chọn điểm đến"
+							onChange={(option)=>{this.renderPriceBenDen(option)}}>
+							<TextInput
+							style={{borderWidth:1, borderColor:'#ccc', padding:10,  width: 200, height:30, marginTop: 10, marginBottom: 10}}
+							editable={false}
+							placeholder="Vui lòng chọn điểm đến"
+							value={currentDiemDen} />
+							</ModalPicker>
+						);
+
+						html.push(
+							<Text key="5" style={{color: 'red', fontSize: 20, marginTop: 10, marginBottom: 20}}>{priceConver} VNĐ</Text>
+						);
+
 						if(type == 'update') {
-							if(totalPriceInt > 0) {
-								currentPrice = totalPriceInt;
-							}
-							if(currentPrice > 0) {
-								priceConver = currentPrice.toFixed(0).replace(/./g, function(c, i, a) {
-									return i && c !== "." && ((a.length - i) % 3 === 0) ? ',' + c : c;
-								});
-							}
+							html.push(
+								<Button key="6" block success onPress={this.updateGiuong.bind(this, this.state.nameGiuong)}>Cập nhật</Button>
+							);
 						}else {
-							if(totalPriceInt > 0) {
-								priceConver = totalPriceInt.toFixed(0).replace(/./g, function(c, i, a) {
-									return i && c !== "." && ((a.length - i) % 3 === 0) ? ',' + c : c;
-								});
-							}
+							html.push(
+								<Button key="6" block success onPress={this.bookGiuong.bind(this, this.state.nameGiuong)}>Book</Button>
+							);
 						}
-					Object.keys(data).map(function(key) {
-						let checkSelect = false;
-						if(keyDiemDi != '' && keyDiemDi == key) {
-							checkSelect = true;
-							if(type == 'update') {
-								currentDiemDi = data[key];
-							}
-						}
-						listItem1.push({key: key.toString(), section: checkSelect, label: data[key], value: key});
-					});
-
-					Object.keys(data).map(function(key) {
-						let checkSelect = false;
-						if(keyDiemDen != '' && keyDiemDen == key) {
-							checkSelect = true;
-							if(type == 'update') {
-								currentDiemDen = data[key];
-							}
-						}
-						listItem2.push({key: key.toString(), section: checkSelect, label: data[key], value: key});
-					});
-
-					html.push(
-						<Text key="1" style={{marginTop: 20, width: 200}}>Điểm đi:</Text>
-					);
-					html.push(
-						<ModalPicker
-						key="2"
-						data={listItem1}
-						initValue="Chọn điểm đi"
-						onChange={(option)=>{this.renderPriceBenDi(option)}}>
-						<TextInput
-						style={{borderWidth:1, borderColor:'#ccc', padding:10,  width: 200, height:30, marginTop: 10, marginBottom: 10}}
-						editable={false}
-						placeholder="Vui lòng chọn điểm đi"
-						value={currentDiemDi} />
-						</ModalPicker>
-					);
-					html.push(
-						<Text key="3" style={{width: 200}}>Điểm đến:</Text>
-					);
-					html.push(
-						<ModalPicker
-						key="4"
-						data={listItem2}
-						initValue="Chọn điểm đến"
-						onChange={(option)=>{this.renderPriceBenDen(option)}}>
-						<TextInput
-						style={{borderWidth:1, borderColor:'#ccc', padding:10,  width: 200, height:30, marginTop: 10, marginBottom: 10}}
-						editable={false}
-						placeholder="Vui lòng chọn điểm đến"
-						value={currentDiemDen} />
-						</ModalPicker>
-					);
-
-					html.push(
-						<Text key="5" style={{color: 'red', fontSize: 20, marginTop: 10, marginBottom: 20}}>{priceConver} VNĐ</Text>
-					);
-
-					if(type == 'update') {
-						html.push(
-							<Button key="6" block success onPress={this.updateGiuong.bind(this, this.state.nameGiuong)}>Cập nhật</Button>
-						);
-					}else {
-						html.push(
-							<Button key="6" block success onPress={this.bookGiuong.bind(this, this.state.nameGiuong)}>Book</Button>
-						);
 					}
-				}
 
+				}
 			}
 		}
 		return html;
