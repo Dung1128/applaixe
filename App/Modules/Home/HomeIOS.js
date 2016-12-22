95d41c1c2a15a66671f8f2c3be5a0046376535af
tuanphan <tuanpm.deverloper@gmail.com>
2016-12-22 14:15:42 +0700

    .

--- App/Modules/Home/HomeIOS.js
+++ App/Modules/Home/HomeIOS.js
@@ -335,7 +335,7 @@ class HomeIOS extends Component {
 			}
       return(
          <View style={[styles.container]}>
-				<View style={{flexDirection: 'row', padding: 30}}>
+				<View style={{flexDirection: 'row', padding: 10}}>
 					<Text style={{flex: 3, width: 200, borderWidth:1, borderColor:'#ccc', padding:10, height:45}} onPress={() => this._setDatePickerShow()}>{this.state.fullDate}</Text>
 					<TouchableOpacity style={{flex: 1, borderRadius: 0, width: 70, backgroundColor: '#1e90ff', height: 45, alignItems: 'center', justifyContent: 'center'}} onPress={() => {this._getNot()}}>
 						<Icon name="ios-search" style={{color: '#fff'}} />
