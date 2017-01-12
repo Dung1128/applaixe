a4fc2c34cc5a3bec9b1a003614149ffe4fb2829b
tuanphan <tuanpm.deverloper@gmail.com>
2017-01-12 13:58:57 +0700

    .

--- App/Config/common.js
+++ App/Config/common.js
@@ -2,7 +2,5 @@
 module.exports = {
   domain: 'http://hasonhaivan.vn',
   // domain: 'http://hai-van.local',
-  cache: 'no-cache',
-  linkApple: 'itms://itunes.apple.com/us/app/lai-xe-ha-son-hai-van/id1184798501?mt=8',
-  linkAndroid: 'market://play.google.com/store'
+  cache: 'no-cache'
 }
